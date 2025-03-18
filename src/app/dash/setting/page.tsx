a54fa128046e18ef8ea5e0b2ff1b920c583b69ea
April <github@sku.moe>
2025-03-18 11:18:28 +0800

    feat: add sidebar in setting page, close VMB-1 (#3)
    * add setting-sidebar
    
    * modify setting-sidebar
    
    * style: remove unnessary change
    
    ---------
    
    Co-authored-by: lucas777778 <3098274296@qq.com>

--- src/app/dash/setting/page.tsx
+++ src/app/dash/setting/page.tsx
@@ -1,3 +1,13 @@
+import { AppSidebar } from "@/components/app-sidebar"
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 export default function SettingPage() {
-  return <div>Setting</div>;
+  return (
+    <SidebarProvider>
+      <AppSidebar/>
+      <SidebarTrigger/>
+      <main>
+        <h1>setting</h1>
+      </main>
+    </SidebarProvider>
+  )
 }
